82e85a2cdd598f3a0017ebda4a9a285ffd3dd644
Ashik Mahmud <ashikmahmud@Ashiks-MacBook-Pro.local>
2021-09-03 20:36:39 +0600

    Default form setup

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import {isLoggedInVar} from "../apollo";
 
 export const LoggedOutRouter = () => {
-    const onClick = () => {
-        isLoggedInVar(true)
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (event: any) => {
+        const {target: {name, value}} = event;
+        if (name === "email") {
+            setEmail(value)
+        }
+        if (name === "password") {
+            setPassword(value);
+        }
     }
 
     return (
         <div>
             <h1>Logged Out</h1>
-            <button onClick={onClick}>Click to login</button>
+            <form>
+                <input
+                    onChange={onChange}
+                    name="email"
+                    type="email"
+                    required
+                    placeholder="email"
+                    value={email}/>
+                <input
+                    onChange={onChange}
+                    name="password"
+                    type="password"
+                    required
+                    placeholder="password"
+                    value={password}/>
+
+            </form>
         </div>
     )
 }
