526d1b592d8ccea08fa6582c5ee2f1bf33b4077d
Ashik Mahmud <ashikmahmud@Ashiks-MacBook-Pro.local>
2021-09-23 21:15:04 +0600

    Added react-hook-form

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,38 +1,47 @@
-import React, {useState} from "react";
-import {isLoggedInVar} from "../apollo";
+import React from "react";
+import {useForm} from "react-hook-form";
+
+
+interface IForm {
+    email: string,
+    password: string
+}
 
 export const LoggedOutRouter = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
 
-    const onChange = (event: any) => {
-        const {target: {name, value}} = event;
-        if (name === "email") {
-            setEmail(value)
-        }
-        if (name === "password") {
-            setPassword(value);
-        }
+    const {register, watch, handleSubmit, formState: {errors}} = useForm<IForm>();
+    const onSubmit = () => {
+        console.log(watch())
+    }
+
+    const onInvalid = () => {
+        console.log("can't create account")
     }
 
     return (
         <div>
             <h1>Logged Out</h1>
-            <form>
-                <input
-                    onChange={onChange}
-                    name="email"
-                    type="email"
-                    required
-                    placeholder="email"
-                    value={email}/>
-                <input
-                    onChange={onChange}
-                    name="password"
-                    type="password"
-                    required
-                    placeholder="password"
-                    value={password}/>
+            <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+                <div>
+                    <input
+                        {...register("email",
+                            {required: true})
+                        }
+                        type="email"
+                        placeholder="email"/>
+                </div>
+                {errors.email && <span className="font-bold text-red-600">Email is required</span>}
+
+                <div>
+                    <input
+                        {...register("password", {required: true})}
+                        name="password"
+                        type="password"
+                        required
+                        placeholder="password"/>
+                </div>
+
+                <input type="submit" value="Submit"/>
 
             </form>
         </div>
